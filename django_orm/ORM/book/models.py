e41f28c017663f0aea7e9cf7ab48fedd43ccbe46
Matias Farias <mat.fariasc@icloud.com>
2021-07-29 16:40:34 -0400

    ..
    ..

--- django_orm/ORM/book/models.py
+++ django_orm/ORM/book/models.py
@@ -15,3 +15,10 @@ class Book(models.Model):
         Author, related_name="books", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Publisher(models.Model):
+    name = models. CharField(max_length=255)
+    books = models. ManyToManyField(Book, related_name="publishers")
+    created_at = models. DateTimeField(auto_now_add=True)
+    updated_at = models. DateTimeField(auto_now=True)
---
+++ django_orm/ORM/books_to_many/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BooksToManyConfig(AppConfig):
+    name = 'books_to_many'
